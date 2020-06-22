650a97a7a5046a437144373b88b7b85345b4fad9
Bryce Pingul <brycepingul@gmail.com>
2020-06-22 13:21:11 -0700

    adding year parameters

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-$(search-button).click(function(){
-    getResults();
+// $(search-button).click(function(){
+//     getResults();
 
-    // title author and url grab
+//     // title author and url grab
 
-});
+// });
 
 function getResults(){
     var userSearchTerm = $("#search-term").val();
@@ -19,11 +19,40 @@ function getResults(){
         method: "GET"
     }).then(function(result){
         console.log(result);
-        var title = result.response.docs[i].headline.main;
+        var title = result.response.docs[3].headline.main;
         console.log(title);
-        var authorName = result.response.docs[i].byline.original;
+        var authorName = result.response.docs[3].byline.original;
         console.log(authorName);
-        var resultUrl = result.response.docs[i].web_url;
+        var resultUrl = result.response.docs[3].web_url;
         console.log(resultUrl);
     });
+}
+
+var startYear = parseInt($("#start-year")) + "0101";
+var endYear = parseInt($("#end-year")) + "0101";
+
+if (startYear === "" && endYear === ""){
+    getResults();
+} else {
+    function getYearResults(){
+        var userSearchTerm = $("#search-term").val();
+        var numInput = parseInt($("#record-num"));
+    
+        var apiKey = "ak1TtO904cMZl4y60G9IkMOSdkawNvWG";
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + "pizza" + "&begin_date=" + startYear + "&end_date=" + endYear +"&api-key=" + apiKey;
+        console.log(queryURL);
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(result){
+            console.log(result);
+            var title = result.response.docs[3].headline.main;
+            console.log(title);
+            var authorName = result.response.docs[3].byline.original;
+            console.log(authorName);
+            var resultUrl = result.response.docs[3].web_url;
+            console.log(resultUrl);
+        });
+    }
 }
