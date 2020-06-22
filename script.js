4c6eb650267552b5b38fe1665d0327fb1d679418
Bryce Pingul <brycepingul@gmail.com>
2020-06-22 13:34:07 -0700

    added year limits

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
 
-// $(search-button).click(function(){
-//     getResults();
-
-//     // title author and url grab
-
-// });
-
-function getResults(){
-    var userSearchTerm = $("#search-term").val();
-    var numInput = parseInt($("#record-num"));
-
-    var apiKey = "ak1TtO904cMZl4y60G9IkMOSdkawNvWG";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + "pizza" + "&api-key=" + apiKey;
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(result){
-        console.log(result);
-        var title = result.response.docs[3].headline.main;
-        console.log(title);
-        var authorName = result.response.docs[3].byline.original;
-        console.log(authorName);
-        var resultUrl = result.response.docs[3].web_url;
-        console.log(resultUrl);
-    });
-}
-
-var startYear = parseInt($("#start-year")) + "0101";
-var endYear = parseInt($("#end-year")) + "0101";
+$('#search-button').click(function(){
+    var startYear = parseInt($("#start-year")) + "0101";
+    var endYear = parseInt($("#end-year")) + "0101";
 
 if (startYear === "" && endYear === ""){
     getResults();
@@ -39,20 +11,68 @@ if (startYear === "" && endYear === ""){
         var numInput = parseInt($("#record-num"));
     
         var apiKey = "ak1TtO904cMZl4y60G9IkMOSdkawNvWG";
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + "pizza" + "&begin_date=" + startYear + "&end_date=" + endYear +"&api-key=" + apiKey;
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearchTerm + "&begin_date=" + startYear + "&end_date=" + endYear +"&api-key=" + apiKey;
         console.log(queryURL);
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(result){
+            $('#search-results').empty();
+        for (let i=0; i<numInput; i++) {
+            console.log(i)
+            //grab results from API
             console.log(result);
-            var title = result.response.docs[3].headline.main;
+            var title = result.response.docs[i].headline.main;
             console.log(title);
-            var authorName = result.response.docs[3].byline.original;
+            var authorName = result.response.docs[i].byline.original;
             console.log(authorName);
-            var resultUrl = result.response.docs[3].web_url;
+            var resultUrl = result.response.docs[i].web_url;
             console.log(resultUrl);
+            //display results to page
+            var displayDiv = $(`<div class="result-div">`);
+            displayDiv.append($('<h3 class="resultTitle">').text(title));
+            displayDiv.append($('<p class="resultBiline">').text(authorName));
+            displayDiv.append($(`<a href="${resultUrl}" target="blank">`).text(`Link to article`));
+            $('#search-results').append(displayDiv);
         });
     }
 }
+});
+
+getResults();
+function getResults(){
+    var userSearchTerm = $("#search-term").val();
+    var numInput = parseInt($("#record-num"));
+
+
+    var apiKey = "ak1TtO904cMZl4y60G9IkMOSdkawNvWG";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearchTerm + "&api-key=" + apiKey;
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(result){
+        $('#search-results').empty();
+        for (let i=0; i<numInput; i++) {
+            console.log(i)
+            //grab results from API
+            console.log(result);
+            var title = result.response.docs[i].headline.main;
+            console.log(title);
+            var authorName = result.response.docs[i].byline.original;
+            console.log(authorName);
+            var resultUrl = result.response.docs[i].web_url;
+            console.log(resultUrl);
+            //display results to page
+            var displayDiv = $(`<div class="result-div">`)
+            displayDiv.append($('<h3 class="resultTitle">').text(title))
+            displayDiv.append($('<p class="resultBiline">').text(authorName))
+            displayDiv.append($(`<a href="${resultUrl}" target="blank">`).text(`Link to article`))
+            $('#search-results').append(displayDiv)
+        }
+    });
+}
+
+
